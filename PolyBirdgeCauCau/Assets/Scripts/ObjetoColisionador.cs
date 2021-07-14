9a0771015c27c230c1f4c9de7078f189a1e6f270
Angelobpixel <68713508+Angelobpixel@users.noreply.github.com>
2021-07-13 22:33:38 -0400

    Cambios0

--- PolyBirdgeCauCau/Assets/Scripts/ObjetoColisionador.cs
+++ PolyBirdgeCauCau/Assets/Scripts/ObjetoColisionador.cs
@@ -6,11 +6,16 @@ public class ObjetoColisionador : MonoBehaviour
 {
     public GameObject objeto;
     public GameObject Panel;
+    public int cant_automoviles;
+    int cont_automoviles_exito = 0;
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.collider == true)
         {
+            cont_automoviles_exito += 1;
             objeto.SetActive(false);
+        }
+        if (cont_automoviles_exito == cant_automoviles) {
             Panel.SetActive(true);
         }
     }
